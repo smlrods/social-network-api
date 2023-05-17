237f6b17ed6fcba5307bf257fa16592eb1cfa5c7
sml <smlrodrigues.dev@gmail.com>
2023-05-17 07:27:58 -0300

    Turn fullname field to virtual

--- src/models/user.js
+++ src/models/user.js
@@ -6,7 +6,12 @@ const UserSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   profile_image: { type: String, required: true },
-  fullname: { type: String, required: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+});
+
+UserSchema.virtual('fullname').get(function () {
+  return `${this.first_name} ${this.last_name}`;
 });
 
 export default mongoose.model('User', UserSchema);
