e9cc6ef999ca7d80319a808fed3cf1ddbe4666b0
sml <smlrodrigues.dev@gmail.com>
2023-06-07 07:44:12 -0300

    Remove default user image

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
-const defaultUserImage =
-  'https://avatars.githubusercontent.com/u/84884629?s=400&u=800be1277201b1f51b9048f404c06e8bf0e5a194&v=4';
+const defaultUserImage = '';
 
 const UserSchema = new Schema({
   username: { type: String, required: true },
